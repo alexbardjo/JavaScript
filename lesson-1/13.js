67bb246029c049ec7fe77449c3fec9229ee3f26b
alexbardjo <55153946+alexbardjo@users.noreply.github.com>
2019-09-22 19:52:45 +0300

    Add files via upload

--- lesson-1/13.js
+++ lesson-1/13.js
@@ -1,4 +1,16 @@
+// let n = 1000;
+// let num = 5;
+//     x = n/(2**num);
+//     console.log(x);
+
 let n = 1000;
-let num = 5;
-    x = n/(2**num);
-    console.log(x);
+Label: while (n >= 50) {
+     let result = n/2;
+     if (result < 50) {
+        console.log(result);
+        break;
+     } else {
+         continue Label;
+     }
+ }
+ 
