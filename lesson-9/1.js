d571b26a819e28b54f5f68d9d137de793388f73d
alexbardjo <55153946+alexbardjo@users.noreply.github.com>
2019-10-19 22:32:49 +0300

    Add files via upload

---
+++ lesson-9/1.js
@@ -0,0 +1,48 @@
+/**
+ * Задача 1.
+ *
+ * Создайте функцию shallowMerge.
+ * Функция обладает двумя параметрами, каждый из которых должен быть обычным JavaScript объектом.
+ * Функция возвращает новый объект, который в себе объединяет свойства обоих объектов.
+ * Свойства необходимо копировать лишь на один уровень каждого их объектов.
+ *
+ * Из объектов и обеих аргументах копируются только собственные свойства, включая недоступные для перечисления.
+ *
+ * Условия:
+ * - Встроенный метод Object.create() использовать запрещено;
+ * - При копировании любого свойства необходимо обязательно сохранить его дескрипторы;
+ * - Свойства с одинаковыми именами нужно перезаписывать — приоритетом обладает объект из второго параметра.
+ */
+
+// Решение
+
+const user = { firstName: 'Marcus', lastName: 'Kronenberg' };
+const userData = { job: 'developer', country: 'Germany', lastName: 'Schmidt' };
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'firstName'));
+
+Object.defineProperty(user, 'firstName', { writable: false });
+Object.defineProperty(userData, 'job', { configurable: false });
+
+
+
+
+ 
+const shallowMerge = function(user, userData) {
+
+    let UserClone = Object.defineProperties({}, Object.getOwnPropertyDescriptors(user));
+
+    let userDataClone = Object.defineProperties(UserClone, Object.getOwnPropertyDescriptors(userData));
+
+    return userDataClone;
+}
+
+
+
+const result = shallowMerge(user, userData);
+
+console.log(result); // { firstName: 'Marcus', lastName: 'Schmidt', job: 'developer', country: 'Germany' }
+console.log(Object.getOwnPropertyDescriptor(result, 'firstName').writable); // false
+console.log(Object.getOwnPropertyDescriptor(result, 'job').configurable); // false
+
+exports.shallowMerge = shallowMerge;
